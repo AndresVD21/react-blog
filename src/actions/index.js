3a3a6bd5e666251148fa5e7c65cfd11225d5f44f
Andres Fernando Valencia Diaz <a.valencia@globant.com>
2019-04-10 17:30:52 -0500

    updating files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,30 @@
-import jsonplaceholder from '../apis/jsonplaceholder'
+import _ from 'lodash';
+import jsonplaceholder from '../apis/jsonplaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+        await dispatch(fetchPosts());
+        // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+        // userIds.forEach(id => dispatch(fetchUser(id)));
+
+        _.chain(getState().posts)
+                .map('userId')
+                .uniq()
+                .forEach(id => dispatch(fetchUser(id)))
+                .value();
+};
 
 export const fetchPosts = () => async dispatch => {
         const response = await jsonplaceholder.get('/posts');
 
-        dispatch({type: 'FETCH_POSTS', payload: response})
+        dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
+
+export const fetchUser = (id) => async dispatch => {
+        const response = await jsonplaceholder.get(`/users/${id}`);
+        dispatch({type:'FETCH_USER',payload: response.data});
+};
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//         const response = await jsonplaceholder.get(`/users/${id}`);
+//         dispatch({type:'FETCH_USER',payload: response.data});
+// });
